da6bf12002b6fb6fa0602977a3fcf8df6ce718ff
sonam tshering <sonamt@zoho.com>
2022-03-22 11:09:00 +0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## The function first initializes the set, get, set_inverse and get_inverse methods.
 makeCacheMatrix <- function(x = matrix()) {
+  
+  ## function to check if matrix x is "square and invertible" 
+  check <- function(x){ 
+      if(nrow(x) != ncol(x)) {
+         stop("matrix must be square")
+      }
+  
+      if(det(x) == 0) {
+         stop("matrix is not invertible")
+      }
+  }
+  
+  inv <- NULL
+  set <- function(y){
+    check(y)
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get <- function() {x}
+  set_inverse <- function(inversed) { inv <<- inversed}
+  get_inverse <- function() {inv}
+  
+  list( set = set,
+        get = get,
+        set_inverse = set_inverse,
+        get_inverse = get_inverse)
 
 }
 
 
-## Write a short comment describing this function
-
+## This function solves for the inverse of the matrix and caches it in "inv"
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$get_inverse()
+  
+  if(!is.null(inv)) {
+    message("getting cached data....")
+    return(inv)
+  }
+  
+  mat <- x$get()
+  inv <- solve(mat)
+  set_inverse <- x$set_inverse(inv)
+  
+  return(inv)
 }
